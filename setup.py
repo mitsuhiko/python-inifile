6f549b888c60a498b0981eb538e079561f29d8cf
Armin Ronacher <armin.ronacher@active-4.com>
2015-02-26 22:27:46 +0100

    Fixed up setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup
 
 setup(
     name='inifile',
+    license='BSD',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    description='A small INI library for Python.',
     version='0.1',
     py_modules=['inifile'],
 )
